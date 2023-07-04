7ddf3e88549d8175b4b84ce716bc539907767035
tanmayrane18 <tanmayrane51@gmail.com>
2023-07-04 15:43:55 +0530

    Profile schema added

---
+++ server/models/Profile.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const profileSchema = mongoose.Schema({
+    gender:{
+        type:String,
+    },
+    dateOfBirth:{
+        type:String,
+    },
+    about:{
+        type:String,
+        maxlength:200
+    },
+    favGenre:{
+        type:String,
+    },
+    favAnime:{
+        type:String,
+    },
+    favMovie:{
+        type:String,
+    },
+    favMaleChar:{
+        type:String,
+    },
+    favVillan:{
+        type:String,
+    },
+    favFemaleChar:{
+        type:String,
+    },
+})
+
+module.exports = mongoose.model("profile",profileSchema)
