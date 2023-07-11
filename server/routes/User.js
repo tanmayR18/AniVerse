84231fbb0dad1545c3239cc7c5bdd922bef11576
tanmayrane18 <tanmayrane51@gmail.com>
2023-07-11 14:46:38 +0530

    routes for user authentication and reseting password added

---
+++ server/routes/User.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const router = express.Router()
+
+//fetch all the middlewares
+const {
+    auth,
+    isAdmin,
+    isUser
+} = require("../middlewares/auth")
+
+//fetch all Users controllers
+const {
+    login,
+    signup,
+    sendOTP,
+    changePassword
+} = require("../controllers/Auth")
+
+//fetch all controllers for reset password
+const {
+    resetPassword,
+    resetPasswordToken
+} = require("../controllers/ResetPassword")
+
+
+//***************************************************************//
+//                     Authentication Routes
+//***************************************************************//
+
+router.post("/login",login)
+router.post("/signup",signup)
+router.post("/sendOTP",sendOTP)
+router.post("/changePassword",changePassword)
+
+//***************************************************************//
+//                     Reset-Password Routes
+//***************************************************************//
+
+router.post("reset-password-token",resetPasswordToken)
+router.post("reset-password",resetPassword)
+
+module.exports = router
