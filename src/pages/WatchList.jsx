d8c848703e3f4b810750b44879702b5840e9724b
tanmayrane18 <tanmayrane51@gmail.com>
2023-09-17 11:31:21 +0530

    Add to watchlist page added

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -2,14 +2,49 @@ import React from 'react'
 import NavbarCommonComp from '../components/common/NavbarCommonComp'
 import Navbar from '../components/common/Navbar'
 import { useSelector } from 'react-redux'
+import AnimeCard from '../components/common/AnimeCard'
+import {FaHeart} from "react-icons/fa"
+import notFound from "../assets/noAnimeFound.jpeg"
+import Footer from "../components/common/Footer"
 
 const WatchList = () => {
     const watchList = useSelector( state => state.watchList)
     console.log("Inside the watch list section", watchList.length, watchList)
   return (
-    <div>
+    <div className='flex flex-col items-center'>
         <Navbar bgColor={"bg-richblack-20 backdrop-blur "}  />
         <NavbarCommonComp/>
+
+        {/* Heading */}
+        {
+            watchList.length > 0 && 
+            <h1 className=' flex w-10/12 my-10 text-[2rem] gap-2 items-center text-richwhite-100 font-semibold'>
+                <FaHeart className=' ' />
+                <p>To Watch</p>
+            </h1>
+        }
+        
+        {/* ANime list */}
+        <div className='grid w-10/12 gap-4 mb-10 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {
+                watchList.map( anime => (
+                    <AnimeCard key = {anime.mal_id} anime = {anime} />
+                ))
+            }
+        </div>
+
+        {/* IF there is no anime in list */}
+        {
+            watchList.length === 0 &&
+            <div className=' flex flex-col items-center my-10 gap-4'>
+                <h1 className=' text-richwhite-100 font-bold text-[2rem]'>
+                    OOP's no anime in the watchList
+                </h1>
+                <img className=' aspect-auto h-60 rounded-md' src={notFound} />
+            </div>
+        }
+
+        <Footer/>
     </div>
   )
 }
--- src/slices/watchListSlice.js
+++ src/slices/watchListSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const userFavAnime = localStorage.getItem("favAnime") ? JSON.parse(localStorage.getItem("toWatchAnime")) : []
+const userFavAnime = localStorage.getItem("toWatchAnime") ? JSON.parse(localStorage.getItem("toWatchAnime")) : [];
 
 export const watchListSlice = createSlice({
     name: "watchList",
@@ -8,19 +8,18 @@ export const watchListSlice = createSlice({
     reducers: {
         add: (state, value) => {
             state.push(value.payload)
-            localStorage.setItem("toWatchAnime", JSON.stringify(value.payload))
+            localStorage.setItem("toWatchAnime", JSON.stringify(state))
             // return value.payload
         },
         remove: (state, value) => {
-            // console.log("Data received", value.payload)
-            // const filteredList = state.filter( (item) => (
-            //     item.mal_id !== value.payload.mal_id
-            // ))
-            // console.log("Current state", filteredList)
-            // localStorage.setItem("toWatchAnime", JSON.stringify(state))
+            console.log("Kya aaraaha he ", value.payload)
+            console.log("Current state kya he ", state)
+            const filteredList = state.filter( (item) => (
+                item.mal_id !== value.payload
+            ))
+            state.splice(0, state.length, ...filteredList);
+            localStorage.setItem("toWatchAnime", JSON.stringify(state))
             // return value.payload
-
-            return 
         }
     }
 })
